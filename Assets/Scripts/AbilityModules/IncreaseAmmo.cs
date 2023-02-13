f00429ddb0ac71d83255e4dfd5e27fa2586f975b
tiendat3699 <onesteep00@gmail.com>
2023-02-13 14:58:16 +0700

    add ability module increaseAmmo

---
+++ Assets/Scripts/AbilityModules/IncreaseAmmo.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using MyCustomAttribute;
+
+public class IncreaseAmmo : AbsAbilityModule
+{
+    [SerializeField, Label("Value(%)")] private float value;
+    private AbsPlayerAttack playerAttack;
+    private float preAmmo;
+    public override void AbilityActive()
+    {
+        playerAttack = GetComponent<PlayerController>().GetPlayerAttackComp();
+        preAmmo = playerAttack.MagazineCapacity;
+        playerAttack.MagazineCapacity += Mathf.RoundToInt(preAmmo * (value/100));
+
+    }
+
+    public override AbsAbilityModule AddAbility(GameObject parent)
+    {
+        IncreaseAmmo increaseAmmo = parent.AddComponent<IncreaseAmmo>();
+        increaseAmmo.tier = tier;
+        increaseAmmo.abilityName = abilityName;
+        increaseAmmo.description = description;
+        increaseAmmo.value = value;
+        increaseAmmo.enabled = true;
+        return increaseAmmo;
+    }
+
+    public override void ResetAbility()
+    {
+        
+    }
+
+}
